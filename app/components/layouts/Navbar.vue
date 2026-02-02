8a300a3b04150aad075d297d9ae81e83e2f0e4a7
Lilian Peuron <lilianpeuron@gmail.com>
2026-02-02 15:48:31 +0100

    Upd Navbar style + SearchBar

--- app/components/layouts/Navbar.vue
+++ app/components/layouts/Navbar.vue
@@ -1,9 +1,12 @@
 <template>
-    <nav>
-        <ul class="flex justify-center gap-2 p-4">
-            <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li><NuxtLink to="/Cards">Cards</NuxtLink></li>
-            <li><NuxtLink to="/Games">Games</NuxtLink></li>
+    <nav class="grid grid-cols-3 sticky top-0 bg-white z-50 p-4">
+        <div class="col-start-1"><NuxtLink to="/">Home</NuxtLink></div>
+        <ul class="col-start-2 flex justify-center gap-4">
+            <li><NuxtLink to="/Cards">Search Cards</NuxtLink></li>
+            <li><NuxtLink to="/Games">Play Games</NuxtLink></li>
+        </ul>
+        <ul class="flex justify-end">
+            <li><NuxtLink to="/">À propos</NuxtLink></li>
         </ul>
     </nav>
 </template>
--- app/components/ui/SearchBar.vue
+++ app/components/ui/SearchBar.vue
@@ -7,6 +7,6 @@ const modelValue = defineModel<string>();
     v-model="modelValue"
     type="search"
     placeholder="Rechercher une carte…"
-    class="rounded-full border-2 px-4 py-2 w-full max-w-md"
+    class="rounded-full border-2 border-gray-800 px-4 py-2 w-full max-w-md"
   />
 </template>
