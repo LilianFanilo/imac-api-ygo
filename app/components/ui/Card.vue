ef5545dc7f5da7fddb4f9cd9d449a4a582748fc4
Lilian Peuron <lilianpeuron@gmail.com>
2026-02-13 11:30:52 +0100

    Upd style cards

--- app/components/ui/Card.vue
+++ app/components/ui/Card.vue
@@ -9,19 +9,18 @@
 
 <template>
     <NuxtLink :to="'/Cards/'+ card.id">
-        <div class="grid grid-rows-20 w-80 h-160 bg-white rounded px-4 py-2 outline-4 outline-transparent hover:outline-sky-500 transition-all duration-100">
-            <div class="flex items-center row-span-1">{{ card.name }}</div>
-            <div class="flex items-center row-span-10">
-                <img class="w-72 h-72 rounded" :src="card.card_images[0].image_url_cropped" alt="">
-            </div>
-            <div class="row-span-1">
-                <ul class="text-xs flex gap-x-4">
-                    <li class="border rounded-full px-2 py-1">{{ card.type }}</li>
-                    <li class="border rounded-full px-2 py-1">{{ card.race }}</li>
-                    <li class="border rounded-full px-2 py-1" v-if="card.level">Level {{ card.level }}</li>
+        <div class="flex flex-col justify-between w-80 h-150 bg-white rounded px-4 py-3.5 outline-4 outline-transparent hover:outline-sky-500 transition-all duration-100">
+            <div class="flex flex-col gap-y-2.5">
+                <div class="text-lg font-bold flex items-center">{{ card.name }}</div>
+                <div class="w-72 h-72 bg-cover bg-center rounded" :style="{ backgroundImage: `url(${card.card_images[0].image_url_cropped})` }">
+                </div>
+                <ul class="text-xs flex flex-wrap gap-1">
+                    <li class="flex w-fit h-fit items-center border rounded-full px-2 py-1">{{ card.type }}</li>
+                    <li class="flex w-fit h-fit items-center border rounded-full px-2 py-1">{{ card.race }}</li>
+                    <li class="flex w-fit h-fit items-center border rounded-full px-2 py-1" v-if="card.level">Level {{ card.level }}</li>
                 </ul>
+                <p class="max-h-36 text-xs text-justify line-clamp-9">{{ card.desc }}</p>
             </div>
-            <div class="row-span-6 text-xs text-justify">{{ card.desc }}</div>
             <div v-if="card.atk && card.def" class="row-start-20 text-end">ATK/{{ card.atk }} DEF/{{ card.def}}</div>
             <div v-else-if="card.atk && card.linkval" class="row-start-20 text-end">ATK/{{ card.atk }} LINK-{{ card.linkval }}</div>
         </div>
