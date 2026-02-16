a9513bfef2d2f5d403812631eef34c873a7ac5b0
Lilian Peuron <lilianpeuron@gmail.com>
2026-02-16 23:17:41 +0100

    Cleanup ygoApi.ts: Remove commented lines

--- app/services/api/ygoApi.ts
+++ app/services/api/ygoApi.ts
@@ -1,12 +1,9 @@
-// services/api/ygoApi.ts
 import type { ApiResponse } from "~/types/ygo";
 
-// Note le ": Promise<ApiResponse>" explicite ici
 export const fetchCardsByDate = async (
   start: string,
   end: string,
 ): Promise<ApiResponse> => {
-  // Remplace cette URL par la tienne
   const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?startdate=${start}&enddate=${end}`;
 
   return await $fetch<ApiResponse>(url);
